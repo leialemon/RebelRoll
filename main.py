0960ec5ea59ec2cf224f1b9a5ce4e8d0b2de7059
Juliana Barros <julibarros11@gmail.com>
2024-07-03 17:43:22 +0000

    (FEAT) Bot in working conditions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
-
+import os
+import discord
+from discord.ext import commands
 
 def dados(quantidade):
     vitorias = 0
@@ -22,6 +24,31 @@ def dados(quantidade):
     
     return f'Vitórias: {vitorias} \n Nada: {nada} \n Controles: {controles} \n Entropias: {entropias}' 
     
+    
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+         return
+
+    if message.content.startswith('$d'):
+        await message.channel.send(dados(int(message.content[2:])))
+
+
+token = os.environ['TOKEN']
+client.run(token)
+
 '''
 implementar função de reroll. Input? Criar dicionário com os valores do resultado?
 def reroll(resultado):
