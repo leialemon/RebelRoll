5989123acce60521c811c1f5bc1e40d591101010
Juliana Barros <julibarros11@gmail.com>
2024-07-03 17:36:07 +0000

    Creating a test branch

--- main.py
+++ main.py
@@ -2,31 +2,26 @@ import random
 
 
 def dados(quantidade):
-  vitorias = 0
-  controles = 0
-  nada = 0
-  entropias = 0
-  
-  for i in range(quantidade):
-    dado = random.randint(1,6)
-    match dado:
-      case 1:
-        entropias = entropias + 1
-      case 2 | 3:
-        nada = nada +1
-      case 4 | 5 :
-        controles = controles + 1
-      case 6 :
-        vitorias = vitorias + 1
-      case _ :
-        return
-
-  print(
-    "Vitórias: ", vitorias,
-    "\nControles: ", controles,
-    "\nNada: ", nada,
-    "\nEntropias: ", entropias 
-  )
+    vitorias = 0
+    controles = 0
+    nada = 0
+    entropias = 0
+    for i in range(quantidade):
+        dado = random.randint(1,6)
+        match dado:
+            case 1:
+                entropias = entropias + 1
+            case 2 | 3:
+                nada = nada +1
+            case 4 | 5 :
+                controles = controles + 1
+            case 6 :
+                vitorias = vitorias + 1
+            case _ :
+                return
+    
+    return f'Vitórias: {vitorias} \n Nada: {nada} \n Controles: {controles} \n Entropias: {entropias}' 
+    
 '''
 implementar função de reroll. Input? Criar dicionário com os valores do resultado?
 def reroll(resultado):
@@ -34,4 +29,3 @@ def reroll(resultado):
     case "Vitórias":
 '''
   
-dados(5)
