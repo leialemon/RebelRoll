8ad952971e33094c177ddb44f8ce3cd13bba8b4f
Juliana Barros <julibarros11@gmail.com>
2024-07-03 12:30:50 +0000

    (FEAT) Added the discord bot commands

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
-import random
+import discord
+import os
+from funcoes import dados
 
+intents = discord.Intents.default()
+intents.message_content = True
 
-def dados(quantidade):
-  vitorias = 0
-  controles = 0
-  nada = 0
-  entropias = 0
-  
-  for i in range(quantidade):
-    dado = random.randint(1,6)
-    match dado:
-      case 1:
-        entropias = entropias + 1
-      case 2 | 3:
-        nada = nada +1
-      case 4 | 5 :
-        controles = controles + 1
-      case 6 :
-        vitorias = vitorias + 1
-      case _ :
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
         return
 
-  print(
-    "Vitórias: ", vitorias,
-    "\nControles: ", controles,
-    "\nNada: ", nada,
-    "\nEntropias: ", entropias 
-  )
-'''
-implementar função de reroll. Input? Criar dicionário com os valores do resultado?
-def reroll(resultado):
-  match resultado:
-    case "Vitórias":
-'''
-  
-dados(5)
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('*d'):
+      quantidade = int(message.content.split('*d')[1])
+      dados(quantidade)
+      
+      
+      
+token = os.environ['TOKEN']
+client.run(token)
